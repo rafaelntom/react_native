7febfc15fa7c8a84929839466e4992b7ac3bb543
RafaelTomazini <rafaelnt83@gmail.com>
2024-08-06 08:38:39 -0300

    Folder Structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,16 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="bg-red-400 p-10 rounded-full">
-        This is the index screen!
-      </Text>
+      <Text className="font-psemibold">This is the index asdasdasdas!</Text>
+      <Link
+        href="/home"
+        className="text-blue-200 font-bold mt-3 p-3 bg-neutral-800 rounded-full"
+      >
+        Go to the Home Page
+      </Link>
     </View>
   );
 }
